98bb3d301002ce7b47c41e3909c7facc43b57f66
Yash Choudhary <yashc331@gmail.com>
2022-07-28 20:15:21 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,50 @@
+let songs = [
+    {
+        name: 'Earth',
+        path: 'assets/songs/Song 1.mp3',
+        artist: 'K-391',
+        cover: 'assets/images/earth.jpg'
+    },
+    {
+        name: 'Cradles',
+        path: 'assets/songs/Song 2.mp3',
+        artist: 'Sub Urban',
+        cover: 'assets/images/cradles.jpg'
+    },
+    {
+        name: 'Superhero',
+        path: 'assets/songs/Song 3.mp3',
+        artist: 'Unknown Brain',
+        cover: 'assets/images/superhero.jpg'
+    },
+    {
+        name: 'Dreams',
+        path: 'assets/songs/Song 4.mp3',
+        artist: 'Lost Sky',
+        cover: 'assets/images/dreams.jpg'
+    },
+    {
+        name: 'Heroes Tonight',
+        path: 'assets/songs/Song 5.mp3',
+        artist: 'Janji',
+        cover: 'assets/images/heroes tonight.jpg'
+    },
+    {
+        name: 'On & On',
+        path: 'assets/songs/Song 6.mp3',
+        artist: 'Cartoon',
+        cover: 'assets/images/on&on.jpg'
+    },
+    {
+        name: 'Invisible',
+        path: 'assets/songs/Song 7.mp3',
+        artist: 'ZeusxCrona & JuliusDreisig',
+        cover: 'assets/images/invisible.jpg'
+    },
+    {
+        name: 'Spirit',
+        path: 'assets/songs/Song 8.mp3',
+        artist: 'PLEEG',
+        cover: 'assets/images/spirit.jpg'
+    },
+]
